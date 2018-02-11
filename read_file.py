c7b30aaebb00cb4588b07029d16e229e3c6e0697
jlk368 <jlk368@cornell.edu>
2018-02-10 19:42:00 -0500

    DATASET_MGR: fix errors and python version compatibility issues

--- read_file.py
+++ read_file.py
@@ -9,30 +9,30 @@ class DATASET_MGR:
         byte = self.data_file.read(4)    # bytes for no of images
 
         # convert the data to a valid number
-        data_size = int(byte[0]) << 24 | int(byte[1]) << 16 | int(byte[2]) << 8 | int(byte[3])
+        data_size = ord(byte[0]) << 24 | ord(byte[1]) << 16 | ord(byte[2]) << 8 | ord(byte[3])
 
         # read image height
         byte = self.data_file.read(4)    # bytes for no of images
-        image_rows = int(byte[0]) << 24 | int(byte[1]) << 16 | int(byte[2]) << 8 | int(byte[3])
+        image_rows = ord(byte[0]) << 24 | ord(byte[1]) << 16 | ord(byte[2]) << 8 | ord(byte[3])
         
         # read image width
         byte = self.data_file.read(4)    # bytes for no of images
-        image_cols = int(byte[0]) << 24 | int(byte[1]) << 16 | int(byte[2]) << 8 | int(byte[3])
+        image_cols = ord(byte[0]) << 24 | ord(byte[1]) << 16 | ord(byte[2]) << 8 | ord(byte[3])
         
         # calcualte data dimension
         data_dim = image_rows * image_cols   # data dimension is total no of pixels in size
         self.data_array = np.zeros((data_size, data_dim))    # rows: no of images, cols: feature dimension
 
     def load_data(self):
-        byte = 20
         image_index = 0 # index for image in numpy array
         pixel_index = 0 # index for pixel in numpy array
-        max_image_index, max_pixel_index = self.data_array.shape()
+        max_image_index, max_pixel_index = self.data_array.shape
 
         # start traversing the file
         for image_index in xrange(max_image_index):
             for pixel_index in xrange(max_pixel_index):
-                self.data_array[image_index][pixel_index] = self.data_file.read(1)  # read pixel of 1 byte
+                byte = self.data_file.read(1)  # read pixel of 1 byte
+                self.data_array[image_index, pixel_index] = float(ord(byte))
                 
 
     def print_data(self):
