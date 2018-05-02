7dcd757b3f7c65bcad1f7d2a38fed78f7a5309fa
jlk368 <jlk368@cornell.edu>
2018-05-02 19:48:42 -0400

    few changes to the svm_model regarding data preproc

--- svm_model.py
+++ svm_model.py
@@ -15,7 +15,7 @@ class SVM_Model:
         if datafiles_path == "":
             datafiles_path = "."
 
-        datafile_fullpath = datafiles_path + "/processed_data_image.npy"
+        datafile_fullpath = datafiles_path + "/processed_for_svm.npy"
         labelfile_fullpath = datafiles_path + "/processed_data_label.npy"
 
         # load the image data and labels and pre-process them
@@ -24,7 +24,8 @@ class SVM_Model:
 
         # The raw image data would be an array of 3D images
         # This needs some pre-processing and vectorization
-        self.data = self.pre_process_image_data(raw_image_data)
+        #self.data = self.pre_process_image_data(raw_image_data)
+        self.data = raw_image_data
 
         # similarly for the labels
         self.labels = self.pre_process_labels(raw_labels)
@@ -32,37 +33,36 @@ class SVM_Model:
 
     def pre_process_image_data(self, raw_image_data):
 
-        num_data_points, image_depth, image_width, image_height = raw_image_data.shape
+	num_data_points, image_depth, image_width, image_height = raw_image_data.shape
 
         # load the spiralling order from the csv file
 
-        #os.system('gcc -o spiralling ./spiralling.c')
-        #os.system('./spiralling ' + str(image_width) + ' > ./spiralling.csv')
+        os.system('gcc -o spiralling ./spiralling.c')
+        os.system('./spiralling ' + str(image_width) + ' > ./spiralling.csv')
 
-        #contents = []
-        #f = open('./spiralling.csv')
-        #h = csv.reader(f)
+        contents = []
+        f = open('./spiralling.csv')
+        h = csv.reader(f)
 
-        #for r in h:
-            #contents.append(int(r[0]))
+        for r in h:
+            contents.append(int(r[0]))
 
-        #f.close()
+        f.close()
 
         # Select the middle slice and vectorize the image
 
         processed_data = np.zeros((num_data_points, image_width * image_height))
 
-        #remapping = np.array(contents)
+        remapping = np.array(contents)
 
         for i in range(num_data_points):
 
             raw_image = raw_image_data[i, (image_depth / 2)]
-            #raw_image = np.clip(raw_image, 0, 255)
-            #raw_image = raw_image / 255
+            raw_image = np.clip(raw_image, 0, 255)
+            raw_image = raw_image / 255
 
             flattened = raw_image.flatten()
-            #processed_data[i] = flattened[remapping]
-            processed_data[i] = flattened
+            processed_data[i] = flattened[remapping]
 
         return processed_data
 
